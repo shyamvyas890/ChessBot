b6280ac5e943a7ab36e6079aa456926cee90c51d
Shyam Vyas <44505988+shyamvyas890@users.noreply.github.com>
2024-06-02 20:18:02 -0700

    Updated pretty_print_board to only print last 64 bits if bitboard is larger than 64 bits

--- server/DebuggingTools.py
+++ server/DebuggingTools.py
@@ -1,11 +1,21 @@
 def pretty_print_board(bitboard):
     board = [bit for bit in format(bitboard, '064b')]
     print("---------------------------------")
-    count = 0
-    while(count<64):
-        row = "| "
-        for i in range(8):
-            row += f"{board[count]} | "
-            count=count+1
-        print(row)
-        print("---------------------------------")
+    if(len(board) == 64):
+        count = 0    
+        while(count<64):
+            row = "| "
+            for i in range(8):
+                row += f"{board[count]} | "
+                count=count+1
+            print(row)
+            print("---------------------------------")
+    elif (len(board) > 64):
+        count = len(board)-64
+        while(count<64):
+            row = "| "
+            for i in range(8):
+                row += f"{board[count]} | "
+                count=count+1
+            print(row)
+            print("---------------------------------")
